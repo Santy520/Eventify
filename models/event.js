e56d2779d758400a4dcbb88dc76eb26b9ee33bb9
Gibin <gibinmgeorge2001@gmail.com>
2024-04-02 17:52:52 -0500

    model- starter code

---
+++ models/event.js
@@ -0,0 +1,27 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../');
+
+const Event = sequelize.define('Event', {
+  title: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  location: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATEONLY,
+    allowNull: false
+  },
+  time: {
+    type: DataTypes.TIME,
+    allowNull: false
+  }
+});
+
+module.exports = Event;
