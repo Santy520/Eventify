2af3699fba3181162668e3881937b36b2cfacaba
Ryan M <ryan.a.moises@gmail.com>
2024-04-01 21:36:07 -0400

    Server.js done; TODO: connection.js

--- server.js
+++ server.js
@@ -1,45 +1,47 @@
-// const path = require('path');
-// const express = require('express');
-// const session = require('express-session');
-// const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
-// const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require('./config/connection');
 
-// const app = express();
-// const PORT = process.env.PORT || 3001;
+// for cookies
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-// // Set up Handlebars.js engine with custom helpers
-// const hbs = exphbs.create({ helpers });
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {
-//     maxAge: 300000,
-//     httpOnly: true,
-//     secure: false,
-//     sameSite: 'strict',
-//   },
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
 
-// app.use(session(sess));
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // expires after 1 day
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.use(session(sess));
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-// app.use(routes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening'));
-// });
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
