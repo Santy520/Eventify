e9c03f230ebc6576fa1548bad87def6704040543
Ryan M <ryan.a.moises@gmail.com>
2024-04-04 20:43:21 -0400

    Initial commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,15 +2,16 @@ const router = require('express').Router();
 // const { Event, User } = require('../models');
 
     // Get Request for homepage
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   try {
     // GET all posted events
-      const eventDataPull = await Event.findall({
+      // const eventDataPull = await Event.findall({
         // include
-      });
-      const eventData = eventDataPull.map((event) => event.get({ plain: true }));
+      // });
 
-    res.render('homepage', {eventData});
+      // const event = eventDataPull.map((event) => event.get({ plain: true }));
+
+    res.render('homepage');
 
   } catch (err) {
     res.status(500).json(err);
