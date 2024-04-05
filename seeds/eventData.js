5ab49a06b771f6975f2c27f3da345e9782fed967
Ryan M <ryan.a.moises@gmail.com>
2024-04-04 20:04:19 -0400

    Basic schema/seed code implement

--- seeds/eventData.js
+++ seeds/eventData.js
@@ -2,25 +2,25 @@ const { Event } = require('../models');
 
 const eventData = [
   {
-    "title": "dummy1Title",
-    "description": "dummy1Desc",
-    "location": "dummy1Location",
-    "date": "dummy1Date",
-    "time": "dummy1Time"
+    title: "dummy1Title",
+    description: "dummy1Desc",
+    location: "dummy1Location",
+    date: "2024-05-01",
+    time: "00:00:00"
   },
   {
-    "title": "dummy2Title",
-    "description": "dummy2Desc",
-    "location": "dummy2Location",
-    "date": "dummy2Date",
-    "time": "dummy2Time"
+    title: "dummy2Title",
+    description: "dummy2Desc",
+    location: "dummy2Location",
+    date: "2024-05-02",
+    time: "00:00:00"
   },
   {
-    "title": "dummy3Title",
-    "description": "dummy3Desc",
-    "location": "dummy3Location",
-    "date": "dummy3Date",
-    "time": "dummy3Time"
+    title: "dummy3Title",
+    description: "dummy3Desc",
+    location: "dummy3Location",
+    date: "2024-05-03",
+    time: "00:00:00"
   }
 ]
 
--- seeds/userData.js
+++ seeds/userData.js
@@ -2,19 +2,19 @@ const { User } = require('../models');
 
 const userData = [
   {
-    "name": "dummy1",
-    "email": "dummy1@dummy.com",
-    "password": "dummy1"
+    name: "dummy1",
+    email: "dummy1@dummy.com",
+    password: "dummy1"
   },
   {
-    "name": "dummy2",
-    "email": "dummy2@dummy.com",
-    "password": "dummy2"
+    name: "dummy2",
+    email: "dummy2@dummy.com",
+    password: "dummy2"
   },
   {
-    "name": "dummy3",
-    "email": "dummy3@dummy.com",
-    "password": "dummy3"
+    name: "dummy3",
+    email: "dummy3@dummy.com",
+    password: "dummy3"
   }
 ]
 
