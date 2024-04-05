f5391cdad51ba4149aa4a78d6adde17cc7edca4e
Gibin <gibinmgeorge2001@gmail.com>
2024-04-04 19:58:39 -0500

    added userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,22 +1,52 @@
 const router = require ('express').Router();
+const bcrypt = require('bcrypt');
 const { User } = require('../../models');
 
 // login user set handlebar login flags to on
 
-// LOGIC HERE
-
-// create new user
-router.post('/', async (req, res) => {
+router.post('/login', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const { email, password } = req.body;
+    // Find user by email
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    // Compare passwords
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    // Set session variables
+    req.session.save(() => {
+      req.session.user_id = user.id;
+      req.session.logged_in = true;
+      res.status(200).json({ user_id: user.id });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
+
+// Create new user
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Create user
+    const userData = await User.create({ username, email, password: hashedPassword });
+    // Set session variables
     req.session.save(() => {
       req.session.user_id = userData.id;
       req.session.logged_in = true;
-
       res.status(200).json(userData);
     });
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
+
+module.exports = router;
