1387b02001239f103fd17ca3f5973d87dab306f8
Ryan M <ryan.a.moises@gmail.com>
2024-04-04 20:05:59 -0400

    Small fixes

--- seeds/seed.js
+++ seeds/seed.js
@@ -15,16 +15,3 @@ const seedDatabase = async () => {
 
 // Execute the seed function
 seedDatabase();
-
-
-    // // Loop through eventData and create events with random user_id
-    // for (const event of eventData) {
-    //   // Generate a random user_id from the users array
-    //   const randomUserId = users[Math.floor(Math.random() * users.length)].id;
-
-    //   // Create event with random user_id
-    //   await Event.create({
-    //     ...event,
-    //     user_id: randomUserId,
-    //   });
-    // }
