87c5545346d77257f5a3e7329999c3613528739e
Ryan M <ryan.a.moises@gmail.com>
2024-04-08 20:42:41 -0400

    Delete button for events working properly

--- controllers/api/eventRoutes.js
+++ controllers/api/eventRoutes.js
@@ -5,40 +5,41 @@ const { Event } = require('../../models');
 
 // Post event details -
 router.post('/', async (req, res) => {
-    try {
-        const newEvent = await Event.create({
-            title: req.body.eventTitle,
-            description: req.body.eventDesc,
-            location: req.body.eventLoca,
-            date: req.body.eventDate,
-            time: req.body.eventTime,
-            // user_id: req.session.user_id, PLEASE REVISIT IN FUTURE
-        });
-        res.status(200).json(newEvent);
+  try {
+    const newEvent = await Event.create({
+      title: req.body.eventTitle,
+      description: req.body.eventDesc,
+      location: req.body.eventLoca,
+      date: req.body.eventDate,
+      time: req.body.eventTime,
+      // user_id: req.session.user_id, PLEASE REVISIT IN FUTURE
+    });
+
+    res.status(200).json(newEvent);
+
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
-router.get('/', (req, res) => {
-    try {
+// Delete event
+router.delete('/:id', async (req, res) => {
+  try {
+    const eventDelete = await Event.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
   
-      // if (req.session.logged_in != true) {
-      //   res.redirect('/login');
-      //   return;
-      // }
-      // GET all posted events
-        // const eventDataPull = await Event.findall({
-          // include
-        // });
-  
-        // const event = eventDataPull.map((event) => event.get({ plain: true }));
-  
-      res.render('Events');
-  
-    } catch (err) {
-      res.status(500).json(err);
+    if (!eventDelete) {
+      res.status(404).json({ message: 'No project found with this id!' });
+      return;
     }
-  });
+
+    res.status(200).json(eventDelete);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+})
 
 module.exports = router;
