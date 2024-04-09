3d6bcade8edce7dfda722e14b87d1edbbfa8aa26
Santy520 <sspalacio20@gmail.com>
2024-04-08 20:13:11 -0400

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,22 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306
+//     }
+//   );
+// }
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -9,12 +25,12 @@ if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
-    }
+      port: 3306,
+    },
   );
 }
 
